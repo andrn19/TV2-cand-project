3c0df7e579b7ecd4ada6ef065c6592126769d94f
Jacob <jacob-dan@hotmail.com>
2024-03-11 14:16:46 +0100

    feat: Dummy service for testing

--- Backend/DummyDataService/Controllers/DummyController.cs
+++ Backend/DummyDataService/Controllers/DummyController.cs
@@ -1,11 +1,12 @@
-using ClassLibrary.Classes;
-using ClassLibrary.Interfaces;
+using TV2.ClassLibrary.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using TV2.ClassLibrary.Classes;
+
 namespace DummyDataService.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class DummyController : ControllerBase, IDataService
+public class DummyController : ControllerBase, IMetadataService
 {
     private readonly ILogger<DummyController> _logger;
 
@@ -16,28 +17,33 @@ public class DummyController : ControllerBase, IDataService
     }
 
     [HttpGet]
-    bool IBaseService.Get()
+    public bool Get()
     {
         throw new NotImplementedException();
-    }
-
-    [HttpGet]
-    public Uri? AddMetadata(Metadata metadata)
-    {
-        throw new NotImplementedException();
-    }
-
-    [HttpGet("dummy-string/{number}")]
-    public string? GetMetadataString(int number)
-    {
-        var data = new Metadata(Guid.Empty, "Metadata", "Data has id: " + number);
-        return data.Description;
     }
     
-    [HttpGet("dummy-data/{id}")]
-    public Metadata? GetMetadata(Guid id)
+    [HttpGet]
+    public string? AddMetadata(Metadata metadata)
     {
-        var data = new Metadata(id, "Metadata", "Data has id: " + id);
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet("dummy-string/{id}")]
+    public string? GetMetadataString(string id)
+    {
+        var data = new Metadata(Guid.Parse(id), "Metadata", "Data has id: " + Guid.Parse(id));
+        return data.Description;
+    }
+
+    [HttpGet("dummy-data/{id}")]
+    public Metadata? GetMetadata(string id)
+    {
+        var data = new Metadata(Guid.Parse(id), "Metadata", "Data has id: " + Guid.Parse(id));
         return data;
+    }
+
+    public bool RemoveMetadata(string id)
+    {
+        throw new NotImplementedException();
     }
 }
