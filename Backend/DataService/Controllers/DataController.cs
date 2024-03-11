da87538945035dfc8a0d1eec247c969a53b6aa56
Jacob <jacob-dan@hotmail.com>
2024-03-11 14:15:09 +0100

    feat: basic DataService to for handling metadata hosts

--- Backend/DataService/Controllers/DataController.cs
+++ Backend/DataService/Controllers/DataController.cs
@@ -1,6 +1,48 @@
-namespace DataService.Controllers;
+using TV2.ClassLibrary.Classes;
+using TV2.ClassLibrary.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
-public class DataController
+namespace TV2.DataService.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class DataController : ControllerBase, IMetadataEndpointService, IMetadataEndpointResolver
 {
+    [HttpGet]
+    public bool Get()
+    {
+        return true;
+    }
+
+    [HttpPut("CreateEndpoint")]
+    public bool CreateEndpoint(MetadataHost host)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("UpdateEndpoint")]
+    public bool UpdateEndpoint([FromBody] MetadataHost host)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("DeleteEndpoint/{id}")]
+    public bool DeleteEndpoint(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("ListEndpoints")]
+    public IEnumerable<MetadataHostIdentifier> ListEndpoints()
+    {
+        
+        throw new NotImplementedException();
+    }
     
+    [HttpPost("ResolveEndpoint")]
+    public Task<Uri> Resolve([FromBody] MetadataHostIdentifier identifier)
+    {
+        throw new NotImplementedException();
+    }
 }
