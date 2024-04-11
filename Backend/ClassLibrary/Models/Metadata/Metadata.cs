935bada9d14e6bd5c6d5312a643de309b022efb6
Jacob <jacob-dan@hotmail.com>
2024-04-11 15:03:59 +0200

    feat: metadata model

--- Backend/ClassLibrary/Models/Metadata/Metadata.cs
+++ Backend/ClassLibrary/Models/Metadata/Metadata.cs
@@ -4,11 +4,27 @@ using System.Text.Json.Serialization;
 
 public class Metadata
 {
+    [JsonPropertyName("faces")]
+    public List<Face>? Faces { get; set; }
+    
+    [JsonPropertyName("topics")]
+    public List<Topic>? Topics { get; set; }
+    
+    [JsonPropertyName("labels")]
+    public List<Label>? Labels { get; set; }
+    
     [JsonPropertyName("keywords")]
     public List<Keyword>? Keywords { get; set; }
-
-    /*
-    [JsonPropertyName("topics")]
-    public List<string>? Topics { get; set; }
-    */
+    
+    [JsonPropertyName("namedPeople")]
+    public List<NamedPerson>? NamedPeople { get; set; }
+    
+    [JsonPropertyName("namedLocations")]
+    public List<NamedLocation>? NamedLocations { get; set; }
+    
+    [JsonPropertyName("shots")]
+    public List<Keyframe>? Keyframes { get; set; }
+    
+    [JsonPropertyName("transcript")]
+    public List<Transcript>? Transcripts { get; set; }
 }
