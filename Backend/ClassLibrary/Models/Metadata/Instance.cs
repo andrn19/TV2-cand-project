52de11e444c9425653ae88b895e44dacebc087e6
Jacob <jacob-dan@hotmail.com>
2024-04-11 14:35:14 +0200

    feat: instance

--- Backend/ClassLibrary/Models/Metadata/Instance.cs
+++ Backend/ClassLibrary/Models/Metadata/Instance.cs
@@ -1,6 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace TV2.Backend.ClassLibrary.Models.Metadata;
 
 public class Instance
 {
+    [JsonPropertyName("thumbnailId")]
+    public Guid? ThumbnailId { get; set; }
     
+    [JsonPropertyName("start")]
+    public string Start { get; set; }
+    
+    [JsonPropertyName("end")]
+    public string End { get; set; }
 }
