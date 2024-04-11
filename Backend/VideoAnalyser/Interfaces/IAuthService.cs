f0ebc20b41dc5ac594af298c998d11c549628078
Jacob <jacob-dan@hotmail.com>
2024-04-11 13:40:05 +0200

    feat: dependency injection

--- Backend/VideoAnalyser/Interfaces/IAuthService.cs
+++ Backend/VideoAnalyser/Interfaces/IAuthService.cs
@@ -4,6 +4,19 @@ namespace TV2.Backend.Services.VideoAnalyser.Interfaces;
 
 public interface IAuthService
 {
-    Task<string> AuthenticateAsync();
-    Task<Account> GetAccountAsync(string accountName, string armAccessToken);
+    /// <summary>
+    /// Get Information about the Account
+    /// </summary>
+    /// <returns>An ARM access token.</returns>
+    Task<string> AuthenticateAsync(string armAccessToken);
+    
+    
+    
+    /// <summary>
+    /// Get Information about the Account
+    /// </summary>
+    /// <param name="accountName">The name of the account.</param>
+    /// <param name="armAccessToken">An ARM access token.</param>
+    /// <returns>An account object</returns>
+    Task<Account?> GetAccountAsync(string accountName, string armAccessToken);
 }
