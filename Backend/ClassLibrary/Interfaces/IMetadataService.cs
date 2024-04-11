c96dcb64cae008b2be12c11cafe0d3286b962291
Jacob <jacob-dan@hotmail.com>
2024-04-11 17:45:47 +0200

    feat: update interfaces

--- Backend/ClassLibrary/Interfaces/IMetadataService.cs
+++ Backend/ClassLibrary/Interfaces/IMetadataService.cs
@@ -4,7 +4,7 @@ using Models.Metadata;
 
 public interface IMetadataService : IBaseService
 {
-    public bool AddMetadata(Guid endpoint, Metadata metadata);
+    bool AddMetadata(Guid endpoint, Metadata metadata);
     //public Metadata? GetMetadata(string id);
     //public bool RemoveMetadata(string id);
 }
--- Backend/ClassLibrary/Interfaces/IVideoAnalyserService.cs
+++ Backend/ClassLibrary/Interfaces/IVideoAnalyserService.cs
@@ -1,6 +1,9 @@
 namespace TV2.Backend.ClassLibrary.Interfaces;
 
+using Models.Metadata;
+
 public interface IVideoAnalyserService
 {
-    
+    Task<string> UploadFootage(string footageUrl, string footageName);
+    Task<Metadata> GetMetadata(string footageId);
 }
