e2f710f41b14d95176f57ffc51f12de55cfba0da
Jacob <jacob-dan@hotmail.com>
2024-04-11 14:33:21 +0200

    feat: add metadata models

---
+++ Backend/ClassLibrary/Models/Metadata/Label.cs
@@ -0,0 +1,6 @@
+namespace TV2.Backend.ClassLibrary.Models.Metadata;
+
+public class Label
+{
+    
+}
--- Backend/ClassLibrary/Models/Metadata/Metadata.cs
+++ Backend/ClassLibrary/Models/Metadata/Metadata.cs
@@ -12,30 +12,3 @@ public class Metadata
     public List<string>? Topics { get; set; }
     */
 }
-
-public class Keyword
-{
-    [JsonPropertyName("id")]
-    public int Id { get; set; }
-    
-    [JsonPropertyName("text")]
-    public string Text { get; set; }
-    
-    [JsonPropertyName("confidence")]
-    public double Confidence { get; set; }
-    
-    [JsonPropertyName("language")]
-    public string Language { get; set; }
-    
-    [JsonPropertyName("instances")]
-    public List<Instance> Instances { get; set; }
-}
-
-public class Instance
-{
-    [JsonPropertyName("start")]
-    public string Start { get; set; }
-    
-    [JsonPropertyName("end")]
-    public string End { get; set; }
-}
