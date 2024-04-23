06eac4819c2d0d6b91a091c9892e5cec48d8b78e
Andreas <andrn19@student.sdu.dk>
2024-04-23 19:56:56 +0200

    setup for unit tests, upload file unit test

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/gateway/list-endpoints': 'http://localhost:52001'
-    }
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './tests/setup.ts',
   },
   css: {
     postcss: {
