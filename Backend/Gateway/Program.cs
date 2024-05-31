6421dc5e6f8673cdc17b74694283198d5194e5ad
Jacob <jacob-dan@hotmail.com>
2024-05-31 12:50:00 +0200

    feat: update concurrency test

--- Backend/Gateway/Program.cs
+++ Backend/Gateway/Program.cs
@@ -1,3 +1,5 @@
+using Ocelot.Administration;
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +21,7 @@ builder.Services.AddCors(options =>
 });
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration).AddAdministration("/administration", "secret");
 
 var app = builder.Build();
 var logger = app.Services.GetService<ILogger<Program>>();
